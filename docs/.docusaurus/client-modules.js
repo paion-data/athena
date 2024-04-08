d384addf8a701b05caf261431abe7c488171c019
A-Little-Excited <3030036858@qq.com>
2024-04-08 15:35:47 +0800

    fix-newInstance

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/apple/Desktop/athena/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/apple/Desktop/athena/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/apple/Desktop/athena/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/apple/Desktop/athena/docs/src/css/custom.css"),
-];
