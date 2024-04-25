ad298fc9cce3aefab13975d544f83d869776f4a6
Jiaqi Liu <jack20220723@gmail.com>
2024-04-25 14:06:35 +0800

    Put test to right location (#47)

--- athena-core/src/test/groovy/com/paiondata/athena/file/identifier/FileIdGeneratorFactorySpec.groovy
+++ athena-core/src/test/groovy/com/paiondata/athena/file/identifier/FileIdGeneratorFactorySpec.groovy
@@ -13,9 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.paiondata.athena.file
-
-import com.paiondata.athena.file.identifier.FileIdGeneratorFactory
+package com.paiondata.athena.file.identifier
 
 import spock.lang.Specification
 
